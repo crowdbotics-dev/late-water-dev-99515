0549c1df262cedbd044873722e96af47b321b616
Crowdbotics <engineering@crowdbotics.com>
2023-07-10 12:33:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "late-water-dev-99515",
+  url: "https://late-water-dev-99515.botics.co",
+  api: "https://late-water-dev-99515.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
